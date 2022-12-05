ece11aafe267dd5ec4b34bd001d2f6d0d499eed9
Florin <pricopiflorin@yahoo.com>
2022-12-05 17:11:07 +0200

    Update 1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
+import Problem1.Problems;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        System.out.println("Alege un exercitiu(1 - 5): ");
+        Scanner alegeEx = new Scanner(System.in);
+        int exercitiu = alegeEx.nextInt();
+        Problems.AlegeExercitiu(exercitiu);
     }
+
+
 }
--- src/Problem1/Problems.java
+++ src/Problem1/Problems.java
@@ -1,2 +1,53 @@
-package Problem1;public class Problems {
+package Problem1;
+
+public class Problems {
+
+    public static void AlegeExercitiu (int a) {
+
+        switch (a) {
+            case 1:
+                System.out.println("Exercitiul 1");
+                Exercitiul1();
+                break;
+            case 2:
+                System.out.println("Exercitiul 2");
+                Exercitiul2();
+                break;
+            case 3:
+                System.out.println("Exercitiul 3");
+                Exercitiul3();
+                break;
+            case 4:
+                System.out.println("Exercitiul 4");
+                Exercitiul4();
+                break;
+            case 5:
+                System.out.println("Exercitiul 5");
+                Exercitiul5();
+                break;
+            default:
+                System.out.println("Poti alege doar exercitiile intre valorile 1 si 5.");
+                break;
+        }
+    }
+
+    public static void Exercitiul1 () {
+
+    }
+
+    public static void Exercitiul2 () {
+
+    }
+
+    public static void Exercitiul3 () {
+
+    }
+
+    public static void Exercitiul4 () {
+
+    }
+
+    public static void Exercitiul5 () {
+
+    }
 }
