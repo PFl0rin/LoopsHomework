f8793d29ac4c6d06eb7807002907ec3da558a5d8
Florin <pricopiflorin@yahoo.com>
2022-12-05 18:36:23 +0200

    Update 2.3

--- src/Problem3/Problem3.java
+++ src/Problem3/Problem3.java
@@ -4,15 +4,15 @@ import java.util.Scanner;
 
 public class Problem3 {
     public static void Exercitiul3(int n) {
-        System.out.println("Introduceti un numar pozitiv: ");
-        Scanner numar = new Scanner(System.in);
-        int nr = numar.nextInt();
-        if (nr >= 1) {
-            for (int i = 0; i < 10; i++) {
-                System.out.println(nr + " * " + (i + 1) + " = " + (nr * (i + 1)));
-            }
-        } else {
-            System.out.println("Trebuie sa alegi un numar pozitiv.");
+        System.out.println("Scrie un sir de numere: ");
+        Scanner numere = new Scanner(System.in);
+        int sirDeNumere = numere.nextInt();
+        int numarIntors =0;
+        while( sirDeNumere != 0 ) {
+            numarIntors = numarIntors * 10;
+            numarIntors = numarIntors + sirDeNumere%10;
+            sirDeNumere = sirDeNumere/10;
         }
+        System.out.println("Numarul introdus intors este: " + numarIntors);
     }
 }
